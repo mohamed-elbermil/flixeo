1a19d2c08f4c610c8c8bfb6a2d62eb33f00f4689
mohamed-elbermil <melbermil@outlook.fr>
2026-01-29 10:50:27 +0100

    update TopMovie and add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import TopBar from "@/components/layout/TopBar"
 import HeroSection from "@/components/movie/HeroSection";
+import SideBar from "@/components/layout/SideBar";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
       <TopBar></TopBar>
+      <SideBar></SideBar>
       <HeroSection></HeroSection>
     </div>
   );
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Link from "next/link"
+
+export default function SideBar() {
+  return (
+    <aside className="sidebar-left">
+        <p>texte</p>
+        <p>texte</p>
+        <p>texte</p>
+        <p>texte</p>
+        <p>texte</p>
+    </aside>
+  )
+}
