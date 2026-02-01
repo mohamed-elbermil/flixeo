cae1e8090cd1ac6794a438f26a858549bcd4d982
mohamed-elbermil <melbermil@outlook.fr>
2026-02-01 20:02:12 +0100

    adding the icon on the sidebar left

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,11 +5,9 @@ import Link from "next/link"
 export default function SideBar() {
   return (
     <aside className="sidebar-left">
-        <p>texte</p>
-        <p>texte</p>
-        <p>texte</p>
-        <p>texte</p>
-        <p>texte</p>
+        <i className="fa-solid fa-heart"></i>
+        <i className="fa-solid fa-clock-rotate-left"></i>
+        <i className="fa-solid fa-face-grin-hearts"></i>
     </aside>
   )
 }
